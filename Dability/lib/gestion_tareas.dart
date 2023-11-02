3c4d88aff3735c694a6099098b3325ddc6eebb58
Ana López <77020767+anlop31@users.noreply.github.com>
2023-11-02 10:30:09 +0100

    Ajustes diseño gestión tareas

--- Dability/lib/gestion_tareas.dart
+++ Dability/lib/gestion_tareas.dart
@@ -5,11 +5,16 @@ class GestionTareas extends StatefulWidget {
 }
 
 class _GestionTareasState extends State<GestionTareas> {
+  /*
   final ButtonStyle style = ElevatedButton.styleFrom(
-    // minimumSize: Size(180, 150),
+    minimumSize: Size(MediaQuery.of(context).size.width - 20, 150), // 180, 150
     textStyle: const TextStyle(fontSize: 20),
-    //alignment: Alignment.center,
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+    ),
+    primary: Colors.teal,
   );
+  */
 
   final List<Color> containerColors = [Colors.red, Colors.green, Colors.blue]; // Colores de los contenedores
 
@@ -26,7 +31,16 @@ class _GestionTareasState extends State<GestionTareas> {
             Container(
               alignment: Alignment.center,
               child: ElevatedButton(
-                style: style,
+                style: ElevatedButton.styleFrom(
+                  minimumSize: Size(double.infinity, 150), // 180, 150
+                  textStyle: const TextStyle(fontSize: 40,
+                    color: Colors.black,),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+                  ),
+                  primary: Color(0xFF4A6987),
+                  padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
+                ),
                 onPressed: () {
                   // Acción cuando se presiona el primer botón
                 },
@@ -34,26 +48,53 @@ class _GestionTareasState extends State<GestionTareas> {
                   textAlign: TextAlign.center,),
               ),
             ),
-            Column(
-              children: List.generate(
-                3,
-                  (index) {
-                    return ElevatedButton(
-                      style: style,
-                      onPressed: () {
-                        // acción
-                      },
-                      child: Text('Tarea' + index.toString(),
-                        textAlign: TextAlign.center,),
-                    );
-                  }
-              )
+            SizedBox(height: 60,),
+            Container(
+              decoration: BoxDecoration(
+                color: Color(0xFF4A6987),
+                borderRadius: BorderRadius.circular(30),
+              ),
+              width: MediaQuery.of(context).size.width - 30,
+              padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
+              child:
+                Column(
+                    children: [
+                      SizedBox(height: 30,),
+                      ...List.generate(
+                        3,
+                        (index) {
+                          return Column(
+                            children: [
+
+                              ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  minimumSize: Size(double.infinity, 150), // 180, 150
+                                  textStyle: const TextStyle(fontSize: 40,
+                                    color: Colors.black,),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+                                  ),
+                                  primary: Color(0xFFF5F5F5),
+                                  padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
+                                ),
+                                onPressed: () {
+                                  // acción
+                                },
+                                child: Text('Tarea ' + index.toString(),
+                                  textAlign: TextAlign.center,),
+                              ),
+
+                              SizedBox(height: 30),
+
+                            ],
+                          );
+                        }
+                      )
+                  ]
+                ),
             ),
           ],
         ),
-
-
-
       ),
     );
   }
