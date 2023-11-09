78de1ec44430f0c8b880214c8ef54b99fd86bf7f
Ana López <77020767+anlop31@users.noreply.github.com>
2023-11-09 09:25:03 +0100

    Arreglo interfaz gestion tareas

--- Dability/lib/gestion_tareas.dart
+++ Dability/lib/gestion_tareas.dart
@@ -26,135 +26,143 @@ class _GestionTareasState extends State<GestionTareas> {
       ),
       body:
       Container(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Container(
-              alignment: Alignment.center,
-              child: ElevatedButton(
-                style: ElevatedButton.styleFrom(
-                  minimumSize: Size(double.infinity, 80), // 180, 150
-                  textStyle: const TextStyle(fontSize: 30,
-                    color: Colors.black,),
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+        child: Container(
+          // padding: EdgeInsets.symmetric(vertical: 40),
+          padding: EdgeInsets.only(top: 45.0, bottom: 10.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(horizontal: 14),
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                    minimumSize: Size(double.infinity, 80), // 180, 150
+                    textStyle: const TextStyle(fontSize: 30,
+                      color: Colors.black,),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+                    ),
+                    primary: Color(0xFF4A6987),
+                    padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
                   ),
-                  primary: Color(0xFF4A6987),
-                  padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
-                ),
-                onPressed: () {
-                  // Irá a la pantalla de añadir tarea
-                  // Navigator.push(
-                  //   context,
-                  //   MaterialPageRoute(builder: (context) => AddTask()),
-                  // );
-                },
-                child: Text('Añadir tarea',
-                  textAlign: TextAlign.center,),
-              ),
-            ),
-            SizedBox(height: 60,),
-            Container(
-              decoration: BoxDecoration(
-                color: Color(0xFF4A6987),
-                borderRadius: BorderRadius.circular(30),
-              ),
-              width: MediaQuery.of(context).size.width - 30,
-              padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
-              child: SingleChildScrollView(
-                child: Column(
-                    children: [
-                      SizedBox(height: 30,),
-                      ...List.generate(
-                        7,
-                        (index) {
-                          return Column(
-                            children: [
-
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  minimumSize: Size(double.infinity, 80), // 180, 150
-                                  textStyle: const TextStyle(fontSize: 20,
-                                    color: Colors.black,),
-                                  shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
-                                  ),
-                                  primary: Color(0xFFF5F5F5),
-                                  padding: EdgeInsets.symmetric(horizontal: 20), // Margen horizontal del texto
-                                ),
-                                onPressed: () {
-                                  // acción
-                                },
-                                child:
-                                  Row(
-                                    children: [
-                                      Text(
-                                        'Tarea ' + index.toString(),
-                                        //textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                          color: Colors.black, // Cambia el color del texto a rojo
-                                        ),
-                                      ),
-
-                                      /////
-                                      Align(
-                                        alignment: Alignment.centerRight,
-                                        child: Container(
-                                          //color: Colors.blue,
-                                          child: Row(
-                                            children: [
-
-                                              ElevatedButton(
-                                                onPressed: () {
-                                                  // Acción del botón
-                                                },
-                                                style: ElevatedButton.styleFrom(
-                                                  minimumSize: Size(10, 20),
-                                                  primary: Color(0xFF4A6987)
-                                                ),
-                                                child: Icon(
-                                                    Icons.edit,
-                                                ),
-                                              ),
-
-                                              SizedBox(width: 10,), // cambiar?
-                                              ElevatedButton(
-                                                onPressed: () {
-                                                  // Acción del botón
-                                                },
-                                                style: ElevatedButton.styleFrom(
-                                                    minimumSize: Size(10, 20),
-                                                    primary: Color(0xFF4A6987)
-                                                ),
-                                                child: Icon(
-                                                  Icons.delete,
-                                                ),
-                                              ),
-
-                                            ],
-                                          ),
-                                        ),
-                                      ),
-                                      /////
-                                    ]
-                                  ),
-
-
-
-                              ),
-
-                              SizedBox(height: 30),
-
-                            ],
-                          );
-                        }
-                      )
-                  ]
+                  onPressed: () {
+                    // Irá a la pantalla de añadir tarea
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => AddTask()),
+                    // );
+                  },
+                  child: Text('Añadir tarea',
+                    textAlign: TextAlign.center,),
                 ),
               ),
-              //////
-            ),
-          ],
+              SizedBox(height: 60,),
+              Expanded(
+                child:
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xFF4A6987),
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    height: 400,
+                    width: MediaQuery.of(context).size.width - 30,
+                    padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
+                      child: ListView(
+                          children: [
+                            SizedBox(height: 30,),
+                            ...List.generate(
+                              7,
+                              (index) {
+                                return Column(
+                                  children: [
+
+                                    ElevatedButton(
+                                      style: ElevatedButton.styleFrom(
+                                        minimumSize: Size(double.infinity, 80), // 180, 150
+                                        textStyle: const TextStyle(fontSize: 20,
+                                          color: Colors.black,),
+                                        shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+                                        ),
+                                        primary: Color(0xFFF5F5F5),
+                                        padding: EdgeInsets.symmetric(horizontal: 20), // Margen horizontal del texto
+                                      ),
+                                      onPressed: () {
+                                        // acción
+                                      },
+                                      child:
+                                        Row(
+                                          children: [
+                                            Text(
+                                              'Tarea ' + index.toString(),
+                                              //textAlign: TextAlign.center,
+                                              style: TextStyle(
+                                                color: Colors.black, // Cambia el color del texto a rojo
+                                              ),
+                                            ),
+
+                                            /////
+                                            Align(
+                                              alignment: Alignment.centerRight,
+                                              child: Container(
+                                                //color: Colors.blue,
+                                                child: Row(
+                                                  children: [
+
+                                                    ElevatedButton(
+                                                      onPressed: () {
+                                                        // Acción del botón
+                                                      },
+                                                      style: ElevatedButton.styleFrom(
+                                                        minimumSize: Size(10, 20),
+                                                        primary: Color(0xFF4A6987)
+                                                      ),
+                                                      child: Icon(
+                                                          Icons.edit,
+                                                      ),
+                                                    ),
+
+                                                    SizedBox(width: 10,), // cambiar?
+                                                    ElevatedButton(
+                                                      onPressed: () {
+                                                        // Acción del botón
+                                                      },
+                                                      style: ElevatedButton.styleFrom(
+                                                          minimumSize: Size(10, 20),
+                                                          primary: Color(0xFF4A6987)
+                                                      ),
+                                                      child: Icon(
+                                                        Icons.delete,
+                                                      ),
+                                                    ),
+
+                                                  ],
+                                                ),
+                                              ),
+                                            ),
+                                            /////
+                                          ]
+                                        ),
+
+
+
+                                    ),
+
+                                    SizedBox(height: 30),
+
+                                  ],
+                                );
+                              }
+                            )
+                        ]
+                      ),
+                    //////
+                  ),
+                /////
+              ),
+            ],
+          ),
         ),
       ),
     );
