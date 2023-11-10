e51fb1ebec792d641d09b2bb10b762d6d03724be
VictorPB <victorperezbarrancodev@gmail.com>
2023-11-10 09:23:58 +0100

    Integrando rama gestion_admin

---
+++ Dability/lib/inicio_admin.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'gestion_tareas.dart';
+
+class InicioAdmin extends StatefulWidget {
+  _InicioAdminState createState() => _InicioAdminState();
+}
+
+class _InicioAdminState extends State<InicioAdmin> {
+  final ButtonStyle style = ElevatedButton.styleFrom(
+    minimumSize: Size(double.infinity, 120), // 180, 150
+    textStyle: const TextStyle(fontSize: 30,
+      color: Colors.black,),
+    shape: RoundedRectangleBorder(
+      borderRadius: BorderRadius.circular(30), // Redondear los bordes del botón
+    ),
+    primary: Color(0xFF4A6987),
+    padding: EdgeInsets.symmetric(horizontal: 40), // Margen horizontal
+  );
+
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Inicio Admin'),
+        backgroundColor: Color(0xFF4A6987),
+      ),
+      body:
+      Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Container(
+                //alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  style: style,
+                  onPressed: () {
+                    // Navigator.push(
+                    //   context,
+                    //   MaterialPageRoute(builder: (context) => GestionAlumnos()),
+                    // );
+                  },
+                 child: Text('GESTIONAR ALUMNOS',
+                   textAlign: TextAlign.center,),
+                ),
+            ),
+            SizedBox(height: 80,),
+            Container(
+                //alignment: Alignment.center,
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: ElevatedButton(
+                  style: style,
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => GestionTareas()),
+                    );
+                  },
+                  child: Text('GESTIONAR TAREAS',
+                    textAlign: TextAlign.center,),
+              ),
+            )
+          ],
+        ),
+
+
+
+      ),
+    );
+  }
+
+}
